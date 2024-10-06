713ab9061160fe208269d0d6a727d6880790e432
Anna Powaska <annapowaska@op.pl>
2024-10-06 08:42:41 +0200

    frf

--- dist/js/script.js
+++ dist/js/script.js
@@ -97,24 +97,18 @@ class Products {
  }
  initAccordion(){
   const thisProduct = this;
-  /* find the clickable trigger (the element that should react 
-to clicking) */
+ 
   const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable)
-  /* START: add event listener to clickable trigger on event 
-click */
-  clickableTrigger.addEventListener('click', function(event) 
-{
-event.preventDefault()
-    /* prevent default action for event */
-    /* find active product (product that has active class) */
-    const activeProduct = document.querySelector(select.all.menuProductsActive)
-    /* if there is active product and it's not 
-    
-thisProduct.element, remove class active from it */
-if(activeProduct !==null && activeProduct !== thisProduct.element){
-  thisProduct.element.classList.remove(classNames.menuProduct.wrapperActive)
-}
-    /* toggle active class on thisProduct.element */
+ 
+      clickableTrigger.addEventListener('click', function(event) 
+    {
+    event.preventDefault()
+      
+        const activeProduct = document.querySelector(select.all.menuProductsActive)
+
+    if(activeProduct !==null && activeProduct !== thisProduct.element){
+      thisProduct.element.classList.remove(classNames.menuProduct.wrapperActive)
+    }
     thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive)
   });
 }
