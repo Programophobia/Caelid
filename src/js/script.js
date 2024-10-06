30c09736c96cab9bae91fed69847c013b8a5b1a3
Anna Powaska <annapowaska@op.pl>
2024-10-06 08:51:18 +0200

    r

--- src/js/script.js
+++ src/js/script.js
@@ -95,6 +95,23 @@ class Products {
   const menuContainer = document.querySelector(select.containerOf.menu)
   menuContainer.appendChild(thisProduct.element)
  }
+
+ getElements(){
+      const thisProduct = this;
+      thisProduct.accordionTrigger = 
+    thisProduct.element.querySelector(select.menuProduct.clickable);
+      thisProduct.form = 
+    thisProduct.element.querySelector(select.menuProduct.form);
+      thisProduct.formInputs = 
+    thisProduct.form.querySelectorAll(select.all.formInputs);
+      thisProduct.cartButton = 
+    thisProduct.element.querySelector(select.menuProduct.cartButton);
+      thisProduct.priceElem = 
+    thisProduct.element.querySelector(select.menuProduct.priceElem);
+}
+
+
+
  initAccordion(){
   const thisProduct = this;
  
