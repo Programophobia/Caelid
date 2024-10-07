4c5404498e4ac99214085f17609318fae8e6027a
Anna Powaska <annapowaska@op.pl>
2024-10-07 05:01:54 +0200

    f

--- dist/js/data.js
+++ dist/js/data.js
@@ -111,5 +111,13 @@ dataSource.products = {
       },
     },
   },
-
+  cake: {
+    class: 'small',
+    name: 'Zio Stefano\'s Doughnut',
+    price: 9,
+    description: 'Treat yourself with this soft, freshly baked cookie. The recipe has been handed down from generation to generation in our family and it has won us several first place prizes in local competitions.',
+    images: [
+      '<img class="active" src="images/doughnut.svg">',
+    ],
+  },
 };
