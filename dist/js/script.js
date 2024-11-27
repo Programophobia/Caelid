e08b0f317bbe83d4eb92f8932371107782fdfd4d
Anna Powaska <annapowaska@op.pl>
2024-11-27 05:23:14 +0100

    b

--- dist/js/script.js
+++ dist/js/script.js
@@ -417,7 +417,13 @@ class Cart {
 class CartProducts {
 constructor(menuProduct, element){
   const thisCartProduct = this;
+
 thisCartProduct.id = menuProduct.id;
+thisCartProduct.name = menuProduct.name; 
+thisCartProduct.amount = menuProduct.amount;
+thisCartProduct.priceSingle = menuProduct.priceSingle;
+thisCartProduct.price = menuProduct.price;
+thisCartProduct.params = menuProduct.params;
 }
 
 
